ceaf938f29472c2d4ed9c5b5c762f9c821df7023
Miguel T Rivera <mrivera108@outlook.com>
2019-06-25 23:49:40 -0700

    Users static data

---
+++ src/static_data/users_data.js
@@ -0,0 +1,378 @@
+const users_data = {
+  "users": [
+    {
+      "id": 196035,
+      "username": "dwaynebrock",
+      "name": "Dwayne Brock",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/dwaynebrock\/{size}\/116781_2.png"
+    },
+    {
+      "id": 197255,
+      "username": "cryptographicfool",
+      "name": "cryptographicFool",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/cryptographicfool\/{size}\/118205_2.png"
+    },
+    {
+      "id": 189704,
+      "username": "james922",
+      "name": "James",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/j\/ac91a4\/{size}.png"
+    },
+    {
+      "id": 192827,
+      "username": "reedlex98",
+      "name": "",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/reedlex98\/{size}\/114726_2.png"
+    },
+    {
+      "id": 171209,
+      "username": "silashuereca",
+      "name": "Silas Huereca",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/s\/6bbea6\/{size}.png"
+    },
+    {
+      "id": 198593,
+      "username": "tsunaweak",
+      "name": "Nathaniel Bale\u00f1a",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/tsunaweak\/{size}\/118034_2.png"
+    },
+    {
+      "id": 195737,
+      "username": "acr0matic",
+      "name": "Nikita Mateyuk",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/a\/b782af\/{size}.png"
+    },
+    {
+      "id": 172128,
+      "username": "saclark12000",
+      "name": "",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/s\/aca169\/{size}.png"
+    },
+    {
+      "id": 128909,
+      "username": "Roma",
+      "name": "sjroma@hotmail.com",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/roma\/{size}\/112899_2.png"
+    },
+    {
+      "id": 182969,
+      "username": "brandon_wallace",
+      "name": "",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/brandon_wallace\/{size}\/112535_2.png"
+    },
+    {
+      "id": 177817,
+      "username": "Saiyan",
+      "name": "Stephven Santos",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/s\/ecccb3\/{size}.png"
+    },
+    {
+      "id": 170865,
+      "username": "ieahleen",
+      "name": "ieahleen",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/ieahleen\/{size}\/117811_2.png"
+    },
+    {
+      "id": 163951,
+      "username": "lasjorg",
+      "name": "Lasjorg",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/lasjorg\/{size}\/96853_2.png"
+    },
+    {
+      "id": 1936,
+      "username": "osogrizz",
+      "name": "Osogrizz",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/osogrizz\/{size}\/1284_2.png"
+    },
+    {
+      "id": 193216,
+      "username": "Ananth",
+      "name": "Ananth",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/a\/dfb087\/{size}.png"
+    },
+    {
+      "id": 84340,
+      "username": "owel",
+      "name": "Owel",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/owel\/{size}\/48425_2.png"
+    },
+    {
+      "id": 26219,
+      "username": "tifft12",
+      "name": "tiffany tsai",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/t\/8c91f0\/{size}.png"
+    },
+    {
+      "id": 199004,
+      "username": "albertkao",
+      "name": null,
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/a\/df788c\/{size}.png"
+    },
+    {
+      "id": 9565,
+      "username": "Nicknyr",
+      "name": "Nick",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/nicknyr\/{size}\/66824_2.png"
+    },
+    {
+      "id": 102173,
+      "username": "DivaWeb",
+      "name": "Lanitta ",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/divaweb\/{size}\/64748_2.png"
+    },
+    {
+      "id": 141864,
+      "username": "Gilbert1391",
+      "name": "Gilbert",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/gilbert1391\/{size}\/93761_2.png"
+    },
+    {
+      "id": 184989,
+      "username": "ethanvernon",
+      "name": "Ethan Vernon",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/ethanvernon\/{size}\/110429_2.png"
+    },
+    {
+      "id": 56819,
+      "username": "psychometry",
+      "name": "Psychometry",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/psychometry\/{size}\/79114_2.png"
+    },
+    {
+      "id": 198989,
+      "username": "jjbottero",
+      "name": "",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/j\/ccd318\/{size}.png"
+    },
+    {
+      "id": 198647,
+      "username": "adibafatin",
+      "name": "dba",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/a\/919ad9\/{size}.png"
+    },
+    {
+      "id": 198980,
+      "username": "ccfat",
+      "name": "",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/c\/4491bb\/{size}.png"
+    },
+    {
+      "id": 783,
+      "username": "DanStockham",
+      "name": "Dan Stockham",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/danstockham\/{size}\/111714_2.png"
+    },
+    {
+      "id": 159304,
+      "username": "seinfeld70",
+      "name": "Zullu Balti",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/seinfeld70\/{size}\/117883_2.png"
+    },
+    {
+      "id": 10950,
+      "username": "srichakradhar",
+      "name": "Srichakradhar Reddy",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/srichakradhar\/{size}\/6499_2.png"
+    },
+    {
+      "id": 144830,
+      "username": "nataliecardot",
+      "name": "Natalie Cardot",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/nataliecardot\/{size}\/111888_2.png"
+    },
+    {
+      "id": 198429,
+      "username": "towers1209",
+      "name": "Loreto Espiritu Torres",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/towers1209\/{size}\/117951_2.png"
+    },
+    {
+      "id": 198962,
+      "username": "abdibogor",
+      "name": "Abdibogor",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/a\/e47774\/{size}.png"
+    },
+    {
+      "id": 47540,
+      "username": "JasonXiao2007",
+      "name": "Jason Xiao2007",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/jasonxiao2007\/{size}\/96646_2.png"
+    },
+    {
+      "id": 197909,
+      "username": "SharonNg98",
+      "name": "Sharon Ng",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/sharonng98\/{size}\/117767_2.png"
+    },
+    {
+      "id": 198988,
+      "username": "Kddjs",
+      "name": "Ahmad",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/k\/977dab\/{size}.png"
+    },
+    {
+      "id": 197383,
+      "username": "keithstubbs",
+      "name": "Keith Stubbs",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/keithstubbs\/{size}\/117387_2.png"
+    },
+    {
+      "id": 86412,
+      "username": "dslemay",
+      "name": "Daniel",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/dslemay\/{size}\/117645_2.png"
+    },
+    {
+      "id": 197343,
+      "username": "Ale1",
+      "name": "Alessandro",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/a\/8491ac\/{size}.png"
+    },
+    {
+      "id": 98884,
+      "username": "Cyph09",
+      "name": "Swalehe Mhina",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/cyph09\/{size}\/81770_2.png"
+    },
+    {
+      "id": 166787,
+      "username": "tlc35us",
+      "name": "",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/t\/c77e96\/{size}.png"
+    },
+    {
+      "id": 26669,
+      "username": "guiKailu",
+      "name": "Yann Stoneman (aka Jan Sh\u00e9)",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/guikailu\/{size}\/15211_2.png"
+    },
+    {
+      "id": 167933,
+      "username": "Ahimsaka",
+      "name": "Ahimsaka",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/ahimsaka\/{size}\/102094_2.png"
+    },
+    {
+      "id": 168152,
+      "username": "dingjy94",
+      "name": "Jingyi Ding",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/dingjy94\/{size}\/102518_2.png"
+    },
+    {
+      "id": 166983,
+      "username": "adam-weiler",
+      "name": "Adam",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/adam-weiler\/{size}\/106955_2.png"
+    },
+    {
+      "id": 92150,
+      "username": "jcunanan05",
+      "name": "Jonathan",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/jcunanan05\/{size}\/49787_2.png"
+    },
+    {
+      "id": 196963,
+      "username": "isheetac",
+      "name": "Isheeta",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/i\/ecd19e\/{size}.png"
+    },
+    {
+      "id": 105745,
+      "username": "Dereje1",
+      "name": "Dereje1",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/dereje1\/{size}\/63598_2.png"
+    },
+    {
+      "id": 195104,
+      "username": "sidneyyin",
+      "name": "Sidney Yin",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/sidneyyin\/{size}\/116281_2.png"
+    },
+    {
+      "id": 177911,
+      "username": "Hjb1694",
+      "name": "Hayden",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/hjb1694\/{size}\/106455_2.png"
+    },
+    {
+      "id": 168864,
+      "username": "willjw3",
+      "name": "Will W",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/willjw3\/{size}\/100474_2.png"
+    },
+    {
+      "id": 72050,
+      "username": "itchick85",
+      "name": "Mac",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/itchick85\/{size}\/91919_2.png"
+    },
+    {
+      "id": 190478,
+      "username": "tiroloquo",
+      "name": "tiroloquo",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/t\/3ab097\/{size}.png"
+    },
+    {
+      "id": 197689,
+      "username": "Serj_360",
+      "name": "",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/serj_360\/{size}\/117695_2.png"
+    },
+    {
+      "id": 198994,
+      "username": "mohak1712",
+      "name": "Mohak Puri",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/m\/e5b9ba\/{size}.png"
+    },
+    {
+      "id": 198068,
+      "username": "scottmillion",
+      "name": "Scott Million",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/scottmillion\/{size}\/117766_2.png"
+    },
+    {
+      "id": 198378,
+      "username": "lfac-pt",
+      "name": "Lu\u00eds Cardoso",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/lfac-pt\/{size}\/117917_2.png"
+    },
+    {
+      "id": 198971,
+      "username": "Suu_nazmy",
+      "name": "Suzy nazmy",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/suu_nazmy\/{size}\/118200_2.png"
+    },
+    {
+      "id": 194569,
+      "username": "RohanKumarr",
+      "name": "Rohan Kumar",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/rohankumarr\/{size}\/118199_2.png"
+    },
+    {
+      "id": 139189,
+      "username": "snowmonkey",
+      "name": "Tobias Parent",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/snowmonkey\/{size}\/79328_2.png"
+    },
+    {
+      "id": 132087,
+      "username": "haraprasad45",
+      "name": "haraprasad45@gmail.com",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/h\/8e8cbc\/{size}.png"
+    },
+    {
+      "id": 170915,
+      "username": "Nimp",
+      "name": "Justin Lee Roberts",
+      "avatar_template": "\/forum\/letter_avatar_proxy\/v2\/letter\/n\/7c8e57\/{size}.png"
+    },
+    {
+      "id": 61887,
+      "username": "RandellDawson",
+      "name": "Randell Dawson",
+      "avatar_template": "\/forum\/user_avatar\/www.freecodecamp.org\/randelldawson\/{size}\/56290_2.png"
+    }
+  ]
+}
+
+export default users_data;
