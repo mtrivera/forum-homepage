9251e9bf13512ea17c0cb34fa6b62b0b8623d611
Miguel T Rivera <mrivera108@outlook.com>
2019-06-25 23:50:40 -0700

    Use Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <h1>freeCodeCamp Forum Homepage</h1>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
