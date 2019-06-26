782e3ab1ab6e4bde86a33527f752e89c00c9e4d1
Miguel T Rivera <mrivera108@outlook.com>
2019-06-25 23:49:56 -0700

    Topics static data

---
+++ src/static_data/topics_data.js
@@ -0,0 +1,324 @@
+const topics_data = {
+  "topics": [
+    {
+      "id": 288471,
+      "title": "An icon next to the html5 date input field",
+      "fancy_title": "An icon next to the html5 date input field",
+      "slug": "an-icon-next-to-the-html5-date-input-field",
+      "posts_count": 1,
+      "reply_count": 0,
+      "highest_post_number": 1,
+      "image_url": null,
+      "created_at": "2019-06-23T04:52:49.188Z",
+      "last_posted_at": "2019-06-23T04:52:49.373Z",
+      "bumped": true,
+      "bumped_at": "2019-06-23T04:52:49.373Z",
+      "unseen": false,
+      "pinned": false,
+      "unpinned": null,
+      "visible": true,
+      "closed": false,
+      "archived": false,
+      "bookmarked": null,
+      "liked": null,
+      "views": 2,
+      "like_count": 0,
+      "has_summary": false,
+      "archetype": "regular",
+      "last_poster_username": "albertkao",
+      "category_id": 423,
+      "pinned_globally": false,
+      "featured_link": null,
+      "show_average": false,
+      "has_ratings": false,
+      "has_accepted_answer": false,
+      "posters": [
+        {
+          "extras": "latest single",
+          "description": "Original Poster, Most Recent Poster",
+          "user_id": 199004,
+          "primary_group_id": null
+        }
+      ]
+    },
+    {
+      "id": 288165,
+      "title": "3-4 years into learning web dev and can't land an interview",
+      "fancy_title": "3-4 years into learning web dev and can&rsquo;t land an interview",
+      "slug": "3-4-years-into-learning-web-dev-and-cant-land-an-interview",
+      "posts_count": 8,
+      "reply_count": 2,
+      "highest_post_number": 8,
+      "image_url": null,
+      "created_at": "2019-06-21T23:21:02.818Z",
+      "last_posted_at": "2019-06-23T01:04:19.872Z",
+      "bumped": true,
+      "bumped_at": "2019-06-23T04:40:12.525Z",
+      "unseen": false,
+      "pinned": false,
+      "unpinned": null,
+      "visible": true,
+      "closed": false,
+      "archived": false,
+      "bookmarked": null,
+      "liked": null,
+      "views": 321,
+      "like_count": 3,
+      "has_summary": false,
+      "archetype": "regular",
+      "last_poster_username": "psychometry",
+      "category_id": 299,
+      "pinned_globally": false,
+      "featured_link": null,
+      "show_average": false,
+      "has_ratings": false,
+      "has_accepted_answer": false,
+      "posters": [
+        {
+          "extras": null,
+          "description": "Original Poster",
+          "user_id": 9565,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 102173,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 141864,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 184989,
+          "primary_group_id": null
+        },
+        {
+          "extras": "latest",
+          "description": "Most Recent Poster",
+          "user_id": 56819,
+          "primary_group_id": null
+        }
+      ]
+    },
+    {
+      "id": 288415,
+      "title": "Problems installing Angular",
+      "fancy_title": "Problems installing Angular",
+      "slug": "problems-installing-angular",
+      "posts_count": 2,
+      "reply_count": 0,
+      "highest_post_number": 2,
+      "image_url": null,
+      "created_at": "2019-06-22T22:39:42.279Z",
+      "last_posted_at": "2019-06-23T04:31:47.427Z",
+      "bumped": true,
+      "bumped_at": "2019-06-23T04:31:47.427Z",
+      "unseen": false,
+      "pinned": false,
+      "unpinned": null,
+      "visible": true,
+      "closed": false,
+      "archived": false,
+      "bookmarked": null,
+      "liked": null,
+      "views": 5,
+      "like_count": 0,
+      "has_summary": false,
+      "archetype": "regular",
+      "last_poster_username": "Gilbert1391",
+      "category_id": 421,
+      "pinned_globally": false,
+      "featured_link": null,
+      "show_average": false,
+      "has_ratings": false,
+      "has_accepted_answer": false,
+      "posters": [
+        {
+          "extras": null,
+          "description": "Original Poster",
+          "user_id": 198989,
+          "primary_group_id": null
+        },
+        {
+          "extras": "latest",
+          "description": "Most Recent Poster",
+          "user_id": 141864,
+          "primary_group_id": null
+        }
+      ]
+    },
+    {
+      "id": 288461,
+      "title": "Applied Visual Design: Adjust the Tone of a Color",
+      "fancy_title": "Applied Visual Design: Adjust the Tone of a Color",
+      "slug": "applied-visual-design-adjust-the-tone-of-a-color",
+      "posts_count": 2,
+      "reply_count": 0,
+      "highest_post_number": 2,
+      "image_url": null,
+      "created_at": "2019-06-23T04:04:36.108Z",
+      "last_posted_at": "2019-06-23T04:22:27.593Z",
+      "bumped": true,
+      "bumped_at": "2019-06-23T04:22:27.593Z",
+      "unseen": false,
+      "pinned": false,
+      "unpinned": null,
+      "visible": true,
+      "closed": false,
+      "archived": false,
+      "bookmarked": null,
+      "liked": null,
+      "views": 6,
+      "like_count": 0,
+      "has_summary": false,
+      "archetype": "regular",
+      "last_poster_username": "Gilbert1391",
+      "category_id": 423,
+      "pinned_globally": false,
+      "featured_link": null,
+      "show_average": false,
+      "has_ratings": false,
+      "has_accepted_answer": false,
+      "posters": [
+        {
+          "extras": null,
+          "description": "Original Poster",
+          "user_id": 198647,
+          "primary_group_id": null
+        },
+        {
+          "extras": "latest",
+          "description": "Most Recent Poster",
+          "user_id": 141864,
+          "primary_group_id": null
+        }
+      ]
+    },
+    {
+      "id": 282893,
+      "title": "Web development on QubesOS?",
+      "fancy_title": "Web development on QubesOS?",
+      "slug": "web-development-on-qubesos",
+      "posts_count": 8,
+      "reply_count": 4,
+      "highest_post_number": 8,
+      "image_url": null,
+      "created_at": "2019-06-09T12:45:07.535Z",
+      "last_posted_at": "2019-06-23T04:20:51.730Z",
+      "bumped": true,
+      "bumped_at": "2019-06-23T04:20:51.730Z",
+      "unseen": false,
+      "pinned": false,
+      "unpinned": null,
+      "visible": true,
+      "closed": false,
+      "archived": false,
+      "bookmarked": null,
+      "liked": null,
+      "views": 77,
+      "like_count": 2,
+      "has_summary": false,
+      "archetype": "regular",
+      "last_poster_username": "cryptographicfool",
+      "category_id": 420,
+      "pinned_globally": false,
+      "featured_link": null,
+      "show_average": false,
+      "has_ratings": false,
+      "has_accepted_answer": false,
+      "posters": [
+        {
+          "extras": "latest",
+          "description": "Original Poster, Most Recent Poster",
+          "user_id": 197255,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 182969,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 198980,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 783,
+          "primary_group_id": null
+        }
+      ]
+    },
+    {
+      "id": 210837,
+      "title": "Redux: Remove item from array",
+      "fancy_title": "Redux: Remove item from array",
+      "slug": "redux-remove-item-from-array",
+      "posts_count": 6,
+      "reply_count": 3,
+      "highest_post_number": 8,
+      "image_url": null,
+      "created_at": "2018-07-22T11:52:39.622Z",
+      "last_posted_at": "2019-06-23T03:44:56.267Z",
+      "bumped": true,
+      "bumped_at": "2019-06-23T03:44:56.267Z",
+      "unseen": false,
+      "pinned": false,
+      "unpinned": null,
+      "visible": true,
+      "closed": false,
+      "archived": false,
+      "bookmarked": null,
+      "liked": null,
+      "views": 1585,
+      "like_count": 4,
+      "has_summary": false,
+      "archetype": "regular",
+      "last_poster_username": "DanStockham",
+      "category_id": 421,
+      "pinned_globally": false,
+      "featured_link": null,
+      "show_average": false,
+      "has_ratings": false,
+      "has_accepted_answer": false,
+      "posters": [
+        {
+          "extras": null,
+          "description": "Original Poster",
+          "user_id": 159304,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 10950,
+          "primary_group_id": null
+        },
+        {
+          "extras": null,
+          "description": "Frequent Poster",
+          "user_id": 144830,
+          "primary_group_id": null
+        },
+        {
+          "extras": "latest",
+          "description": "Most Recent Poster",
+          "user_id": 783,
+          "primary_group_id": null
+        }
+      ]
+    }
+  ]
+};
+
+export default topics_data;
