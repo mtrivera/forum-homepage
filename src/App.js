49813b8dca0e6c6550c4dd1d6d3e80f1ffbf7e31
Miguel T Rivera <mrivera108@outlook.com>
2019-07-21 23:46:59 -0700

    Render topic data to screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+// import users from './static_data/users_data';
+import topics from './static_data/topics_data';
+import Posters from './components/Posters';
 
 function App() {
+  
+  // make posterslist component
+  const topicsList = topics.topics.map(topic => {
+    return (
+      <>
+        <li key={topic.title}>
+          <h2>{topic.title}</h2>
+          <h2>Posters: 
+            <Posters 
+              postersData={topic.posters} 
+            />
+          </h2>
+          <h2>Replies: {topic.reply_count}</h2>
+          <h2>Views: {topic.views}</h2>
+        </li>
+      </>
+    )
+  })
+
   return (
     <>
       <Header />
+      { <ul>{topicsList}</ul> }
       <Footer />
     </>
   );
