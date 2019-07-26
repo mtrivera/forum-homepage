438dc453f138175413ed3eb0bf6cbf087bb758bd
Miguel T Rivera <mrivera108@outlook.com>
2019-07-25 23:11:34 -0700

    Render latest post activity

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from "dayjs/plugin/relativeTime";
 import Header from './components/Header';
 import Footer from './components/Footer';
 // import users from './static_data/users_data';
 import topics from './static_data/topics_data';
 import Posters from './components/Posters';
 
+dayjs.extend(relativeTime);
+
 function App() {
-  
+
   // make posterslist component
   const topicsList = topics.topics.map(topic => {
     return (
@@ -20,6 +24,9 @@ function App() {
           </h2>
           <h2>Replies: {topic.reply_count}</h2>
           <h2>Views: {topic.views}</h2>
+          <h2>Activity: 
+            {dayjs(topic.last_posted_at).fromNow()}
+          </h2>
         </li>
       </>
     )
