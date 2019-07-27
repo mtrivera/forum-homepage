7532945e8ddb76f0baeadf91679df7566725a28f
Miguel T Rivera <mrivera108@outlook.com>
2019-07-26 22:17:44 -0700

    Topics component

---
+++ src/components/Topics.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Topic from './Topic';
+import topicData from '../static_data/topics_data';
+
+function Topics(props) {
+  const topicsList = topicData.topics.map(topic => {
+    return (
+      <li>
+        <Topic key={props.id} 
+          data={{topic}}
+        />
+      </li>
+    )
+  })  
+
+  return (
+    <ul>
+      { topicsList }
+    </ul>
+  )
+}
+
+export default Topics;
