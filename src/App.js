2a4310403258761bd6ad59804652ad152bcbb8ee
Miguel T Rivera <mrivera108@outlook.com>
2019-09-12 19:28:21 -0700

    Change to state component with API call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
+import axios from 'axios';
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Topics from './components/Topics';
 
-function App() {
-  return (
-    <>
-      <main className="container">
-        <Header />
-        <Topics />
-        <Footer />
-      </main>
-    </>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    topics: [],
+    isLoaded: false
+  }
+
+  componentDidMount() {
+    this.getAPIData();
+  }
+
+  getAPIData = async () => {
+    let res = await axios.get('https://buttercup-island.glitch.me/latest');
+    let { users } = res.data;
+    let { topics } = res.data.topic_list;
+    this.setState({
+      users,
+      topics,
+      isLoaded: true
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <main className="container">
+          <Header />
+          <Topics />
+          <Footer />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
