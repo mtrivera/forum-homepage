719d4f94ce27eb185f06fb181a532ffe89a852d3
Miguel T Rivera <mrivera108@outlook.com>
2019-09-15 21:59:02 -0700

    Updates

---
+++ precache-manifest.94687ccfa09de540c51ed66bbb396282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951da6447b534cfb2df6a0b73a0eb219",
+    "url": "/forum-homepage/index.html"
+  },
+  {
+    "revision": "64cbe0a13f87fc2d1619",
+    "url": "/forum-homepage/static/css/main.40b870ff.chunk.css"
+  },
+  {
+    "revision": "5cdf39b9f76b46487c53",
+    "url": "/forum-homepage/static/js/2.8f9c731d.chunk.js"
+  },
+  {
+    "revision": "64cbe0a13f87fc2d1619",
+    "url": "/forum-homepage/static/js/main.056b38d0.chunk.js"
+  },
+  {
+    "revision": "4f349060509ac0002265",
+    "url": "/forum-homepage/static/js/runtime~main.5bfbf827.js"
+  }
+]);
